8f634c7e33114e6e4d29d5dadc36343295a2427f
henrique <henrique.florencio307@gmail.com>
2022-06-06 13:41:34 -0300

    infinite scrolling funcionando com svg de carregamento!

--- src/view/Lista/Lista.js
+++ src/view/Lista/Lista.js
@@ -1,112 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import {pegaPokemons} from "../../controller/busca.js"
-import pokedex from 'pokedex-promise-v2';
+import React, { useEffect, useState } from "react";
+import icon from '../assets/loading.svg'
+import { scryRenderedDOMComponentsWithTag } from "react-dom/test-utils";
+// import { pegaPokemons } from "../../controller/busca.js";
+// import pokedex from "pokedex-promise-v2";
 // import getInfo from '../../controller/busca'
-import './index.css'
+import "./index.css";
 
 export default function Lista() {
+  const [pokemonsInPage, setPokemonsInPage] = useState();
+  const [pokemons, setPokemons] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [pesquisa, setPesquisa] = useState();
+  const [selectValue, setSelectValue] = useState("Kanto");
 
-    const [allPokemons, setAllPokemons] = useState()
-    const [pokemons, setPokemons] = useState()
-    const [pesquisa, setPesquisa] = useState()
-    const [selectValue, setSelectValue] = useState('Kanto')
+  useEffect(() => {fetchPokemonsInPage()}, [pageNumber]);
 
-    useEffect(() => fetchAllPokemons,[])
+  useEffect(() => {pegaPokemons()}, [pokemonsInPage]);
 
-    if(!pokemons){
-        pegaPokemon()
-    }
-    
-    useEffect(() => {
-        if(pokemons){
-            const intersectionObserver = new IntersectionObserver((entries) => {
-                
-                if(entries.some((entrie) => entrie.isIntersecting)){
-                    console.log("Estamos Oservando '-' ")
-                }else{
-                    console.log("tamo vendo nada nn")
-                }
-    
-            })
-    
-            intersectionObserver.observe(document.querySelector('.ward'))
-    
-            return () => intersectionObserver.disconnect()    
+  useEffect(() => {
+    if (pokemons.length) {
+        const intersectionObserver = new IntersectionObserver((entries) => {
+        if (entries.some((entrie) => entrie.isIntersecting)) {
+          setPageNumber(pageNumberInsideState => pageNumberInsideState + 1)
         }
-    })
+      });
 
-    // const arrayResult = pokemons.pokemon_species.map(async pokemon => {
-    //     await fetch(pokemon.url).then(res => res.json())
-    // })
-    
-    async function fetchAllPokemons() {
-        await fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=10000')
-        .then(res => res.json())
-        .then(json => setAllPokemons(json))
+      intersectionObserver.observe(document.querySelector(".ward"));
+
+      return () => intersectionObserver.disconnect();
     }
-    
-    async function pegaPokemon() {
-        if(allPokemons) {
-            const array = [] 
-            
-            //allPokemons.results.length
-            // por enquanto só 20 pokemons serão mostrados, ja que demora muito pra mostrar todos =)
-            for(var i=0; i < 30; i++){
-                await fetch(allPokemons.results[i].url)
-                .then(res => res.json())
-                .then(json => array.push(json))
-            }
-            
-            setPokemons(array)
-            //`https://pokeapi.co/api/v2/pokemon/${pesquisa}`
-        }
+  }, [pokemons]);
+
+  async function fetchPokemonsInPage() {
+    await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pageNumber * 30}&limit=${30}`)
+    .then((res) => res.json())
+    .then((newPokemons) => setPokemonsInPage(newPokemons))
+  }
+
+  async function pegaPokemons() {
+    if (pokemonsInPage) {
+      const novos = [];
+      
+      for (var i = 0; i < 30; i++) {
+        await fetch(pokemonsInPage.results[i].url)
+        .then((res) => res.json())
+        .then((json) => novos.push(json));
+      }
+      
+      const antigos = pokemons
+      
+      setPokemons([...antigos, ...novos]);
+      //`https://pokeapi.co/api/v2/pokemon/${pesquisa}`
     }
-    
-    if(!pokemons){
-        return <div>Carregando...</div>
-    } else {
-        return (
-            <>
-                <input 
-                placeholder='Barra de pesquisa'
-                className='pesquisa-box'
-                onChange={(event) => setPesquisa(event.target.value)} />
+}
 
-                <input type='button' value='procura' onClick={pegaPokemon}/>
+if (!pokemons.length) {
+    return <div>Carregando...</div>;
+} else {
+    return (
+        <>
+        <input
+          placeholder="Barra de pesquisa"
+          className="pesquisa-box"
+          onChange={(event) => setPesquisa(event.target.value)}
+        />
 
-                <select onChange={event => setSelectValue(event.target.value)}>
-                    <option value='kanto'>Kanto</option>
-                    <option value='Johto'>Johto</option>
-                    <option value='hoenn'>Hoenn</option>
-                    <option value='sinnoh'>Sinnoh</option>
-                    <option value='unova'>Unova</option>
-                    <option value='kalos'>Kalos</option>
-                    <option value='alola'>Alola</option>
-                    <option value='galar'>Galar</option>
-                </select>
+        <input type="button" value="procura" onClick={pegaPokemons} />
 
-                {/* {
-                    pokemons.filter(pokemon => )
-                } */}
+        <select onChange={(event) => setSelectValue(event.target.value)}>
+          <option value="kanto">Kanto</option>
+          <option value="Johto">Johto</option>
+          <option value="hoenn">Hoenn</option>
+          <option value="sinnoh">Sinnoh</option>
+          <option value="unova">Unova</option>
+          <option value="kalos">Kalos</option>
+          <option value="alola">Alola</option>
+          <option value="galar">Galar</option>
+        </select>
 
-                <ul key={'key'} className='pokelist'>
-                    {/*console.log(pokemon)*/}
-                    {pokemons ? (
-                        pokemons.map((pokemon, index) => {
-                            return(
-                                <>
-                                    <li key={pokemon.name} className='pokecard'>
-                                        <h1>{pokemon.name}</h1>
-                                        <img src={pokemon.sprites.front_default}/>
-                                        <div>ID: {pokemon.id}</div>
-                                    </li>
-                                </>
-                            )
-                        })
-                        ): null}
-                        <li key='justaWard' className='ward'/>
-                </ul>
-            </>
-        )
-    }
+        <ul key={"key"} className="pokelist">
+          {/*console.log(pokemon)*/}
+          {pokemons ? pokemons.map((pokemon, index) => {
+                return (
+                  <>
+                    <li key={pokemon.name} className="pokecard">
+                      <h1>{pokemon.name}</h1>
+                      <img src={pokemon.sprites.front_default} />
+                      <div>ID: {pokemon.id}</div>
+                      <div>{pokemon.types[0].type.name}</div>
+                    </li>
+                  </>
+                );
+              })
+            : null}
+        </ul>
+          <div key="justaWard" className="ward">
+            <img src={icon}/>
+          </div>
+      </>
+    );
+  }
 }
