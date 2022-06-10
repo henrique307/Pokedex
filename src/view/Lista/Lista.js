8fdac86a56fc4b7e34e393aca246c11c9a776721
henrique <henrique.florencio307@gmail.com>
2022-06-10 16:02:11 -0300

    algumas mudanças de design..

--- src/view/Lista/Lista.js
+++ src/view/Lista/Lista.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import icon from '../assets/loading.svg'
+import icon from "../assets/loading.svg";
+import Pesquisa from "../Pesquisa/Pesquisa";
 import { scryRenderedDOMComponentsWithTag } from "react-dom/test-utils";
 // import { pegaPokemons } from "../../controller/busca.js";
 // import pokedex from "pokedex-promise-v2";
 // import getInfo from '../../controller/busca'
 import "./index.css";
+import PesquisaAvancada from "../PesquisaAvancada";
 
 export default function Lista() {
   const [pokemonsInPage, setPokemonsInPage] = useState();
@@ -13,15 +15,19 @@ export default function Lista() {
   const [pesquisa, setPesquisa] = useState();
   const [selectValue, setSelectValue] = useState("Kanto");
 
-  useEffect(() => {fetchPokemonsInPage()}, [pageNumber]);
+  useEffect(() => {
+    fetchPokemonsInPage();
+  }, [pageNumber]);
 
-  useEffect(() => {pegaPokemons()}, [pokemonsInPage]);
+  useEffect(() => {
+    pegaPokemons();
+  }, [pokemonsInPage]);
 
   useEffect(() => {
     if (pokemons.length) {
-        const intersectionObserver = new IntersectionObserver((entries) => {
+      const intersectionObserver = new IntersectionObserver((entries) => {
         if (entries.some((entrie) => entrie.isIntersecting)) {
-          setPageNumber(pageNumberInsideState => pageNumberInsideState + 1)
+          setPageNumber((pageNumberInsideState) => pageNumberInsideState + 1);
         }
       });
 
@@ -32,58 +38,49 @@ export default function Lista() {
   }, [pokemons]);
 
   async function fetchPokemonsInPage() {
-    await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pageNumber * 30}&limit=${30}`)
-    .then((res) => res.json())
-    .then((newPokemons) => setPokemonsInPage(newPokemons))
+    await fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${pageNumber * 30}&limit=${30}`
+    )
+      .then((res) => res.json())
+      .then((newPokemons) => setPokemonsInPage(newPokemons));
   }
 
   async function pegaPokemons() {
     if (pokemonsInPage) {
       const novos = [];
-      
+
       for (var i = 0; i < 30; i++) {
         await fetch(pokemonsInPage.results[i].url)
-        .then((res) => res.json())
-        .then((json) => novos.push(json));
+          .then((res) => res.json())
+          .then((json) => novos.push(json));
       }
-      
-      const antigos = pokemons
-      
+
+      const antigos = pokemons;
+
       setPokemons([...antigos, ...novos]);
       //`https://pokeapi.co/api/v2/pokemon/${pesquisa}`
     }
-}
+  }
 
-if (!pokemons.length) {
+  if (!pokemons.length) {
     return <div>Carregando...</div>;
-} else {
+  } else {
     return (
-        <>
-        <input
-          placeholder="Barra de pesquisa"
-          className="pesquisa-box"
-          onChange={(event) => setPesquisa(event.target.value)}
-        />
-
-        <input type="button" value="procura" onClick={pegaPokemons} />
-
-        <select onChange={(event) => setSelectValue(event.target.value)}>
-          <option value="kanto">Kanto</option>
-          <option value="Johto">Johto</option>
-          <option value="hoenn">Hoenn</option>
-          <option value="sinnoh">Sinnoh</option>
-          <option value="unova">Unova</option>
-          <option value="kalos">Kalos</option>
-          <option value="alola">Alola</option>
-          <option value="galar">Galar</option>
-        </select>
+      <>
+        <Pesquisa setPesquisa={setPesquisa} pegaPokemons={pegaPokemons}/>
+        
+        <PesquisaAvancada setSelectValue={setSelectValue}/>
 
         <ul key={"key"} className="pokelist">
           {/*console.log(pokemon)*/}
-          {pokemons ? pokemons.map((pokemon, index) => {
+          {pokemons
+            ? pokemons.map((pokemon, index) => {
                 return (
                   <>
-                    <li key={pokemon.name} className={`pokecard ${pokemon.types[0].type.name} `} >
+                    <li
+                      key={pokemon.name}
+                      className={`pokecard ${pokemon.types[0].type.name} `}
+                    >
                       <img src={pokemon.sprites.front_default} />
                       <h1>{pokemon.name}</h1>
                       <div>ID: {pokemon.id}</div>
@@ -94,9 +91,9 @@ if (!pokemons.length) {
               })
             : null}
         </ul>
-          <div key="justaWard" className="ward">
-            <img src={icon}/>
-          </div>
+        <div key="justaWard" className="ward">
+          <img src={icon} />
+        </div>
       </>
     );
   }
--- src/view/Pesquisa/Pesquisa.js
+++ src/view/Pesquisa/Pesquisa.js
@@ -1,8 +1,21 @@
-// import React from 'react';
-// import './index.css'
+import React from 'react';
+import './index.css'
 
-// export default function Pesquisa() {
-//     return (
-//         <input placeholder='Barra de pesquisa' className='pesquisa-box' />
-//     )
-// }
+export default function Pesquisa({setPesquisa, pegaPokemons}) {
+    return (
+        <div className='pesquisa'>
+            <input
+            variant="standart"
+            placeholder="Barra de pesquisa"
+            className="pesquisa-field"
+            onChange={(event) => setPesquisa(event.target.value)}
+            />
+            <input type="button" value="Pesquisar!" onClick={pegaPokemons} className="pesquisa-buttom"/>
+            {/* <label class="switch">
+                <label class="label">Pesquisa Avançada</label>
+                <input type="checkbox"/>
+                <span class="slider round"/>
+            </label> */}
+        </div>
+    )
+}
